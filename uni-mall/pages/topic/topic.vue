c556becc77722308838774bafeea8f91b9980bb0
joe <winsfas@163.com>
2024-07-17 17:09:05 +0800

    cartupreload

--- uni-mall/pages/topic/topic.vue
+++ uni-mall/pages/topic/topic.vue
@@ -1,54 +1,60 @@
 <template>
 	<view class="container">
-		<scroll-view v-if="topicList.length > 0" class="topic-list" :scroll-y="true" :scroll-top="scrollTop">
-			<view class="top-row" v-if="topicList.length > 0">
-				<navigator :url="'../topicDetail/topicDetail?id='+topicList[0].id">
-					<image class="top-img" :src="topicList[0].scenePicUrl"></image>
-					<view class="info">
-						<text class="title">{{ topicList[0].title }}</text>
-						<text class="desc">{{ topicList[0].subtitle }}</text>
-						<text class="price">{{ topicList[0].priceInfo }}元起</text>
-					</view>
-				</navigator>
-			</view>top-img
-			<view class="vertical-row" v-if="topicList.length > 2">
-				<view class="vertical-item" v-for="(item, index) in topicList.slice(1, 3)" :key="index">
-					<navigator :url="'../topicDetail/topicDetail?id='+item.id">
-						<image class="vertical-img" :src="item.scenePicUrl"></image>
-						<view class="info">
-							<text class="title">{{ item.title }}</text>
-							<text class="desc">{{ item.subtitle }}</text>
-							<text class="price">{{ item.priceInfo }}元起</text>
-						</view>
-					</navigator>
-				</view>
+	  <scroll-view v-if="topicList.length > 0" class="topic-list" :scroll-y="true" :scroll-top="scrollTop">
+		<view class="top-row" v-if="topicList.length > 0">
+		  <navigator :url="'../topicDetail/topicDetail?id='+topicList[0].id">
+			<view class="item-container">
+			  <image class="top-img" :src="topicList[0].scenePicUrl"></image>
+			  <view class="info">
+				<text class="title">{{ topicList[0].title }}</text>
+				<text class="desc">{{ topicList[0].subtitle }}</text>
+				<text class="price">{{ topicList[0].priceInfo }}元起</text>
+			  </view>
 			</view>
-			<view class="horizontal-section" v-if="topicList.length > 3">
-				<view class="title-row">
-					<text class="official-text">官方严选 超值二手</text>
-					<text class="new-tag">99新</text>
+		  </navigator>
+		</view>
+		<view class="vertical-row" v-if="topicList.length > 2">
+		  <view class="vertical-item" v-for="(item, index) in topicList.slice(1, 3)" :key="index">
+			<navigator :url="'../topicDetail/topicDetail?id='+item.id">
+			  <view class="item-container">
+				<image class="vertical-img" :src="item.scenePicUrl"></image>
+				<view class="info">
+				  <text class="title">{{ item.title }}</text>
+				  <text class="desc">{{ item.subtitle }}</text>
+				  <text class="price">{{ item.priceInfo }}元起</text>
 				</view>
-				<view class="horizontal-items">
-					<view class="horizontal-item" v-for="(item, index) in topicList.slice(3, 6)" :key="index">
-						<navigator :url="'../topicDetail/topicDetail?id='+item.id">
-							<image class="horizontal-img" :src="item.scenePicUrl"></image>
-							<view class="info">
-								<text class="title">{{ item.title }}</text>
-								<text class="desc">{{ item.subtitle }}</text>
-								<text class="price">{{ item.priceInfo }}元起</text>
-							</view>
-						</navigator>
-					</view>
+			  </view>
+			</navigator>
+		  </view>
+		</view>
+		<view class="horizontal-section" v-if="topicList.length > 3">
+		  <view class="title-row">
+			<text class="official-text">官方严选 超值二手</text>
+			<text class="new-tag">99新</text>
+		  </view>
+		  <view class="horizontal-items">
+			<view class="horizontal-item" v-for="(item, index) in topicList.slice(3, 6)" :key="index">
+			  <navigator :url="'../topicDetail/topicDetail?id='+item.id">
+				<view class="item-container">
+				  <image class="horizontal-img" :src="item.scenePicUrl"></image>
+				  <view class="info">
+					<text class="title">{{ item.title }}</text>
+					<text class="desc">{{ item.subtitle }}</text>
+					<text class="price">{{ item.priceInfo }}元起</text>
+				  </view>
 				</view>
+			  </navigator>
 			</view>
-			<view class="page" v-if="showPage">
-				<view :class="'prev ' + (page <= 1 ? 'disabled' : '')" @tap="prevPage">上一页</view>
-				<view :class="'next ' + ((count / size) < page + 1 ? 'disabled' : '')" @tap="nextPage">下一页</view>
-			</view>
-		</scroll-view>
-		<tui-show-empty v-else text="暂无专题"></tui-show-empty>
+		  </view>
+		</view>
+		<view class="page" v-if="showPage">
+		  <view :class="'prev ' + (page <= 1 ? 'disabled' : '')" @tap="prevPage">上一页</view>
+		  <view :class="'next ' + ((count / size) < page + 1 ? 'disabled' : '')" @tap="nextPage">下一页</view>
+		</view>
+	  </scroll-view>
+	  <tui-show-empty v-else text="暂无专题"></tui-show-empty>
 	</view>
-</template>
+  </template>
 
 <script>
 const util = require("@/utils/util.js")
@@ -59,7 +65,7 @@ export default {
         return {
             topicList: [],
             page: 1,
-            size: 10,
+            size: 100,
             count: 0,
             scrollTop: 0,
             showPage: false
@@ -131,126 +137,136 @@ export default {
     }
 }
 </script>
-
 <style lang="scss">
 .container {
-	width: 750rpx;
-	height: 100%;
-	overflow: hidden;
-	background: #100b0b;
+  width: 750rpx;
+  height: 100%;
+  overflow: hidden;
+  background: #100b0b;
 }
 
 .topic-list {
-	width: 750rpx;
-	height: 100%;
-	overflow: hidden;
-	background: linear-gradient(to bottom, url('@/static/images/bg.png') 0% 25%, #fff 25% 50%, url('@/static/images/bg1.png') 50% 75%, #fff 75% 100%), #fff;
-	background-size: 100% 25%;
+  width: 750rpx;
+  height: 100%;
+  overflow: hidden;
+  background: linear-gradient(to bottom, url('@/static/images/bg.png') 0% 25%, #fff 25% 50%, url('@/static/images/bg1.png') 50% 75%, #fff 75% 100%), #fff;
+  background-size: 100% 25%;
 }
 
 .top-row,
 .vertical-row,
 .horizontal-section {
-	background: #fff;
-	margin-bottom: 20rpx;
-	padding: 10rpx;
-	box-sizing: border-box;
+  background: transparent;
+  margin-bottom: 20rpx;
+  padding: 10rpx;
+  box-sizing: border-box;
+}
+
+.item-container {
+  width: 100%;
+  background: transparent;
+  position: relative;
 }
 
 .top-img {
-	width: 50%;
-	height: 400rpx;
-	object-fit: cover;
+  width: 100%;
+  height: 400rpx;
+  object-fit: cover;
 }
 
 .vertical-img,
 .horizontal-img {
-	width: 100%;
-	height: 200rpx;
-	object-fit: cover;
+  width: 100%;
+  height: 200rpx;
+  object-fit: cover;
 }
 
 .info {
-	width: 100%;
-	text-align: center;
-	padding: 10rpx;
+  width: 100%;
+  text-align: center;
+  padding: 10rpx;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
 }
 
 .title {
-	display: block;
-	width: 100%;
-	color: #333;
-	font-size: 28rpx;
-	margin-top: 5rpx;
+  display: block;
+  width: 100%;
+  color: #fff;
+  font-size: 28rpx;
+  margin-top: 5rpx;
 }
 
 .desc {
-	display: block;
-	width: 100%;
-	color: #999;
-	font-size: 24rpx;
-	margin-top: 5rpx;
+  display: block;
+  width: 100%;
+  color: #ddd;
+  font-size: 24rpx;
+  margin-top: 5rpx;
 }
 
 .price {
-	display: block;
-	width: 100%;
-	color: #b4282d;
-	font-size: 24rpx;
-	margin-top: 5rpx;
+  display: block;
+  width: 100%;
+  color: #ffcc00;
+  font-size: 24rpx;
+  margin-top: 5rpx;
 }
 
 .title-row {
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	margin-bottom: 20rpx;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20rpx;
 }
 
 .official-text {
-	font-size: 30rpx;
-	color: #333;
+  font-size: 30rpx;
+  color: #333;
 }
 
 .new-tag {
-	font-size: 24rpx;
-	color: #b4282d;
+  font-size: 24rpx;
+  color: #b4282d;
 }
 
 .horizontal-items {
-	display: flex;
-	justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 
 .horizontal-item {
-	width: 32%;
-	background: #fff;
-	padding: 10rpx;
-	box-sizing: border-box;
+  width: 32%;
+  background: transparent;
+  padding: 10rpx;
+  box-sizing: border-box;
 }
 
 .page {
-	width: 750rpx;
-	height: 108rpx;
-	background: #fff;
-	margin-bottom: 20rpx;
-	display: flex;
-	justify-content: space-between;
+  width: 750rpx;
+  height: 108rpx;
+  background: #fff;
+  margin-bottom: 20rpx;
+  display: flex;
+  justify-content: space-between;
 }
 
 .page .prev,
 .page .next {
-	width: 50%;
-	height: 108rpx;
-	font-size: 29rpx;
-	color: #333;
-	text-align: center;
-	line-height: 108rpx;
-	cursor: pointer;
+  width: 50%;
+  height: 108rpx;
+  font-size: 29rpx;
+  color: #333;
+  text-align: center;
+  line-height: 108rpx;
+  cursor: pointer;
 }
 
 .page .disabled {
-	color: #ccc;
-	cursor: not-allowed;
+  color: #ccc;
+  cursor: not-allowed;
 }
 </style>
